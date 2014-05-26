3dbe78ee44b2adf09e6e3ec636e035e618990dff
Felix Rieseberg <felix.rieseberg@microsoft.com>
2014-05-25 21:22:19 -0700

    CalendarAPI

---
+++ app/apiCalendar.js
@@ -0,0 +1,47 @@
+var gcal = require('google-calendar');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+
+var apiCalendar = function () {
+
+    var self = this;
+
+    this.listCalendars = function (accessToken) {
+        var googleCalendar = new gcal.GoogleCalendar(accessToken);
+    }
+
+    this.newCalendarEntry = function (accessToken, calendarId, text, sendNotification) {
+        EventEmitter.call(this);
+        var googleCalendar = new gcal.GoogleCalendar(accessToken);
+
+        if (calendarId == null || calendarId == "") {
+            calendarId = 'primary';
+        }
+
+        // Text: The text describing the event to be created. (string)
+        //text = text;
+
+        // Whether to send notifications about the creation of the event. Optional. The default is False. (boolean)
+        sendNotification = sendNotification;
+
+        console.log("Trying to create a new event: " + "\n" +
+            "Access Token: " + accessToken + "\n" +
+            "Text: " + text + "\n" +
+            "Notify: " + sendNotification);
+
+        googleCalendar.events.quickAdd(calendarId, text, sendNotification, function (error, data) {
+            if (error) {
+                //self.emit('error', error);
+                console.log(error);
+
+            } else if (data) {
+                //self.emit('success', data);
+                console.log(data);
+            }
+        });
+    };
+
+}
+
+util.inherits(apiCalendar, EventEmitter);
+module.exports = apiCalendar;
