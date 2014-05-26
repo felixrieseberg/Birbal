2226ce00c1f8dd06c8f29a045922c3d2bd75ca34
Arjun Kavi <Arjun@Arjuns-MacBook-Pro-2.local>
2014-05-26 16:08:37 -0700

    Moves Client code under Models

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+   
+    'url' : 'mongodb://birbaluser:XA29PwTeyiiPB@ds027748.mongolab.com:27748/birbalmongo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+       
+};
