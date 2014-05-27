92a19be5d0b84f8a24989fc729e873e62469124d
Felix Rieseberg <felix.rieseberg@microsoft.com>
2014-05-26 17:14:58 -0700

    Changes to user

---
+++ app/capabilities/calendar.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var calendar = new function() {
+    
+    this.createEvent = function (user, option) {
+        
+    }
+
+    this.changeEvent = function (user, option) {
+        
+    }
+
+    this.deleteEvent = function (user, option) {
+        
+    }
+
+}
+
+module.exports = calendar;
