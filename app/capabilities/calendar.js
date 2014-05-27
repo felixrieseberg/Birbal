ee56542329d1c1e1db13fa0e545c31a119bcfad0
Felix Rieseberg <felix.rieseberg@microsoft.com>
2014-05-26 18:21:28 -0700

    Updates to capabilities

--- app/capabilities/calendar.js
+++ app/capabilities/calendar.js
@@ -1,5 +1,6 @@
 var googleCalendar = require('../apiGoogleCalendar.js');
 var q = require(q);
+var errors = require('generics').errors;
 
 var calendar = function () {
 
@@ -8,7 +9,7 @@ var calendar = function () {
         var deferred = q.defer();
         // Do we have access to Google Calendar?
         if (user.google.token == "" || user.google.token != null || user.google.token != undefined) {
-            deferred.reject({ "error": "No Google authorization present", "resolve": "" });
+            deferred.reject({ "error": errors[1], "resolve": "" });
         } else {
             var accessToken = user.google.token;
             var calendarId = options.calendarId;
@@ -17,7 +18,7 @@ var calendar = function () {
             var apiCalendar = new CalendarAPI();
             apiCalendar.newCalendarEntry(accessToken, calendarId, text, function (error, response) {
                 if (error) {
-                    deferred.reject({ "error": "Google unwilling to add event" });
+                    deferred.reject({ "error": errors[2] });
                 } else {
                     deferred.resolve({ "success": "Event created" });
                 }
